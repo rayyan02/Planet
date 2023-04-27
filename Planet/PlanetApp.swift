a9b96d336caf11c174e915da8e8f63084aa7da8e
Rayyan Hasnain <Syed-rayyan.hasnain@lloydsbanking.com>
2023-04-27 12:58:44 +0100

    Initial Commit

---
+++ Planet/PlanetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetApp.swift
+//  Planet
+//
+//  Created by Syed Rayyan Hasnain on 27/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlanetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlanetUITests/PlanetUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PlanetUITestsLaunchTests.swift
+//  PlanetUITests
+//
+//  Created by Syed Rayyan Hasnain on 27/04/2023.
+//
+
+import XCTest
+
+final class PlanetUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
